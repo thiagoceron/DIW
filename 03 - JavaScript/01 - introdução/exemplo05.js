25cf17808228131dba2ef5d40572f43ed16fdace
thiagoceron <thiagoceronalmeida@gmail.com>
2025-06-09 23:20:36 -0300

    novo commit

---
+++ 03 - JavaScript/01 - introdução/exemplo05.js
@@ -0,0 +1,9 @@
+let n = 10
+n += 5
+n *= 5
+n++
+console.log("O resultado do resto divisão de:"+n+" por 6 é: "+(n % 6))
+console.log("O quadrado do resto da divisão de:"+n+" por 6 é: "+(n % 6)**2)
+console.log(n)
+n /= 4
+console.log(n)
---
+++ 03 - JavaScript/01 - introdução/exemplo06.js
@@ -0,0 +1,7 @@
+let num = 10
+console.log(typeof(num))
+num = String(num)
+console.log(typeof(num))
+num = Boolean(num)
+console.log(typeof(num))
+console.log(num)
